f40b7dbb602b169bd4ee4724d54096e0e20403fb
AsifITk <asifkhanit10@gmail.com>
2022-09-14 16:43:35 +0600

    added board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -10,15 +10,32 @@ import SendIcon from '@mui/icons-material/Send';
 import { useRef } from "react";
 import { createList, getBoards } from "../features/dataSlice";
 import { useState } from "react";
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 export default function Board() {
+  const [open, setOpen] = React.useState(false);
   const { userInfo } = useSelector((state) => state.wall);
   const { currBoard } = useSelector((state) => state.cabinet);
   const { id } = useParams();
   const dispatch = useDispatch();
   const listName = useRef(null);
   let [listText, setList] = useState("");
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     dispatch(getSingleBoard(id));
@@ -40,7 +57,22 @@ export default function Board() {
           height: 25,
           fontSize: 10,
           color: 'blue',
-        }} />) : null}
+        }} onClick={() => handleOpen} />) : null}
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
         <Box className="createList" sx={{
 
           display: 'flex',
